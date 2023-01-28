e0ba9b1a316f20ba8597827c080836f792ca4fa9
Chamara-Wijepala <mr.worf777@gmail.com>
2023-01-28 20:14:16 +0530

    Submit exercises 2.11-2.15

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
+import personsService from "./services/persons";
+
 export default function App() {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [nameFilter, setNameFilter] = useState("");
@@ -33,12 +30,53 @@ export default function App() {
     const nameAlreadyExists = persons.some((person) => person.name === newName);
 
     if (nameAlreadyExists) {
-      alert(`${newName} is already added to the phonebook`);
+      const foo = window.confirm(
+        `${newName} is already added to the phonebook, replace the old number with the new one?`
+      );
+
+      if (!foo) return;
+
+      const personToUpdate = persons.find((person) => person.name === newName);
+
+      personsService
+        .update(personToUpdate.id, { ...personToUpdate, number: newNumber })
+        .then((updatedPerson) =>
+          setPersons(
+            persons.map((person) =>
+              person.id === personToUpdate.id ? updatedPerson : person
+            )
+          )
+        );
+
+      setNewName("");
+      setNewNumber("");
     } else {
-      setPersons(persons.concat({ name: newName, number: newNumber }));
+      personsService
+        .create({
+          name: newName,
+          number: newNumber,
+        })
+        .then((newPerson) => setPersons([...persons, newPerson]));
+
       setNewName("");
     }
   };
+
+  const handleDelete = (name, id) => {
+    if (!window.confirm(`Delete ${name} ?`)) return;
+
+    personsService.remove(id).then((status) => {
+      if (status === 200) {
+        setPersons(persons.filter((person) => person.id !== id));
+      }
+    });
+  };
+
+  useEffect(() => {
+    personsService
+      .getAll()
+      .then((initialPersons) => setPersons(initialPersons));
+  }, []);
 
   return (
     <div>
@@ -58,7 +96,7 @@ export default function App() {
 
       <h2>Numbers</h2>
 
-      <Persons personsToShow={personsToShow} />
+      <Persons personsToShow={personsToShow} handleDelete={handleDelete} />
     </div>
   );
 }
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-export default function Persons({ personsToShow }) {
+export default function Persons({ personsToShow, handleDelete }) {
   return (
     <ul>
       {personsToShow.map((person) => (
         <li key={person.name}>
-          {person.name} {person.number}
+          {person.name} {person.number}{" "}
+          <button onClick={() => handleDelete(person.name, person.id)}>
+            delete
+          </button>
         </li>
       ))}
     </ul>
